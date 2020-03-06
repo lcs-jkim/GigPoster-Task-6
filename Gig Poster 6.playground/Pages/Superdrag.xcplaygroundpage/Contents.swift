185a51602010e9bfa0396f67c8bc9fe48ea13dd8
Amanda <jkim@lcs.on.ca>
2020-03-06 13:14:46 -0500

    Subtext size text changed, finished

--- Gig Poster 6.playground/Pages/Superdrag.xcplaygroundpage/Contents.swift
+++ Gig Poster 6.playground/Pages/Superdrag.xcplaygroundpage/Contents.swift
@@ -98,18 +98,19 @@ canvas.textColor = Color.black
 canvas.drawText(message: "superdrag", at: Point(x: -180, y: -318), size: 55, kerning: 0)
 
 // Subtext #1
-canvas.drawText(message: "with", at: Point(x: -180, y: -345), size: 10, kerning: 0)
-canvas.drawText(message: "the shambles", at: Point(x: -180, y: -360), size: 10, kerning: 0)
-canvas.drawText(message: "and lifter", at: Point(x: -180, y: -375), size: 10, kerning: 0)
+canvas.drawText(message: "with", at: Point(x: -180, y: -345), size: 9, kerning: 0)
+canvas.drawText(message: "the shambles", at: Point(x: -180, y: -360), size: 9, kerning: 0)
+canvas.drawText(message: "and lifter", at: Point(x: -180, y: -375), size: 9, kerning: 0)
 
 // Subtext #2
-canvas.drawText(message: "thursday", at: Point(x: -80, y: -345), size: 10, kerning: 0)
-canvas.drawText(message: "june 13 1996 / 8:30", at: Point(x: -80, y: -360), size: 10, kerning: 0)
-canvas.drawText(message: "no age limit", at: Point(x: -80, y: -375), size: 10, kerning: 0)
+canvas.drawText(message: "thursday", at: Point(x: -80, y: -345), size: 9, kerning: 0)
+canvas.drawText(message: "june 13 1996 / 8:30", at: Point(x: -80, y: -360), size: 9, kerning: 0)
+canvas.drawText(message: "no age limit", at: Point(x: -80, y: -375), size: 9, kerning: 0)
 
-canvas.drawText(message: "at brick by brick", at: Point(x: 100, y: -345), size: 10, kerning: 0)
-canvas.drawText(message: "1130 buenos avenue", at: Point(x: 100, y: -360), size: 10, kerning: 0)
-canvas.drawText(message: "san diego, ca", at: Point(x: 100, y: -375), size: 10, kerning: 0)
+//Subtext #3
+canvas.drawText(message: "at brick by brick", at: Point(x: 100, y: -345), size: 9, kerning: 0)
+canvas.drawText(message: "1130 buenos avenue", at: Point(x: 100, y: -360), size: 9, kerning: 0)
+canvas.drawText(message: "san diego, ca", at: Point(x: 100, y: -375), size: 9, kerning: 0)
 
 
 
